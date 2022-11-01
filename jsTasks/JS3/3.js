1a17df0721749003e6c14cf3098e726191b8a987
Shahmirze <sahmirze.memmedyarov@gmail.com>
2022-11-01 22:32:02 +0400

    javascript task 3

---
+++ jsTasks/JS3/3.js
@@ -0,0 +1,16 @@
+//fatorial
+
+let eded=+prompt('nece faktorial olmalidir')
+fktr=1
+for (let i=1;i<=eded;i++){
+	fktr*=i
+}
+alert(fktr)
+
+//natural ededlerin cemi
+let natural=+prompt("edelerin cemi ")
+let tip=0
+for(i=0;i<=natural;i++){
+	tip+=i
+}
+alert(tip)
