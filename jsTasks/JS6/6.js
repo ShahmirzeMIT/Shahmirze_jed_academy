da58e6c7768c3f00c51d2cd1b4330d659551cb2e
Shahmirze <sahmirze.memmedyarov@gmail.com>
2022-11-09 23:00:20 +0400

    jedacademy task 6 Obj :)

--- jsTasks/JS6/6.js
+++ jsTasks/JS6/6.js
@@ -1,30 +1,32 @@
 let Calculator = {
-    read(a, b) {
-        //    a = +prompt("irinici ededi daxil edin")
-        //     b= +prompt("ikinci ededi daxil edin")
-        return a + b
+    read(a = 0, b = 0) {
+        //    //    a = +prompt("irinici ededi daxil edin")
+        //    //     b= +prompt("ikinci ededi daxil edin")
+        //    return a + b
+        Calculator.a = a
+        Calculator.b = b
     },
     sum() {
-        console.log(this.x + this.y)
+        console.log(this.a + this.b)
     },
     multiply() {
-        console.log(this.x * this.y)
+        console.log(this.a * this.b)
     },
     divide() {
-        console.log(this.x / this.y)
+        console.log(this.a / this.b)
     },
     subtract() {
-        console.log(this.x - this.y)
+        console.log(this.a - this.b)
     }
 }
 
 let x = +prompt('birinci edidi daxil et')
 let y = +prompt('ikinci edidi daxil et')
-Calculator.x = x
-Calculator.y = y
-
+    // Calculator.x = x
+    // Calculator.y = y
+Calculator.read(x, y)
 Calculator.sum()
 Calculator.multiply()
 Calculator.divide()
 Calculator.subtract()
-console.log(Calculator.x)
+console.log(Calculator.a)
