d4722b0f390fcedc944ca1fd7dad32481e26d67e
Shahmirze <sahmirze.memmedyarov@gmail.com>
2022-11-15 23:18:22 +0400

    Jedacademy task 8 Array

--- jsTasks/JS8/8.js
+++ jsTasks/JS8/8.js
@@ -35,3 +35,36 @@
  }
 
  printWithIndex()
+
+ //eded ve massiv veririk ve bize netice olaraq birinic dxil etdiyimiz qeder massivden eded qaytarir
+ function Count(n,...arrn){
+	for (let i=0; i<n;i++) {
+	console.log(arrn[i])
+	}
+}
+ Count(2,2,3,5,6)
+ //alretnativi slice metodudur
+//  function Count(n=2,...arrn){
+// 	if(arrn.length==0){
+// 		return('arrayi niye bos saxlamisan ')
+// 	}
+// 	let hazir=arrn.slice(0 ,n);
+// 	return arrn
+//  }
+//  console.log(Count(7,{as:'Shahmirze',surname:'Mammadyarli'},1,3,4,[1,3,4,5],3,4,5,7,8,9));
+
+//  // 7
+ //eded ve massiv veririk ve bize netice olaraq birinic dxil etdiyimiz qeder massivin sonundan eded qaytarir
+
+ function lastCount(n,...arrlast){
+	if(arrlast.length==0){
+		return ('A Oho Niye Bos array gosterirsen ?? :<')
+	}
+
+	 for(let i=arrlast.length-1;i>n;i--){
+			console.log(arrlast[i])
+	 }
+ }
+ lastCount(5,22,5,6,7,8,9,0)
+
+ //8
---
+++ jsTasks/JS8/practice/1.js
@@ -0,0 +1,105 @@
+
+
+	// esl
+	// debugger
+	// let arr=[[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]
+
+	// for(let i=0;i<arr.length;i++){
+	// 	console.log(`row ${arr.indexOf(arr[i])}`)
+	// 	for(let j=0;j<arr[i].length;j++){
+	// 		console.log(`${arr[i][j]}`)
+	// 	}
+	// }
+
+	//  Write a JavaScript program to find the sum of squares of a numeric vector
+
+	// function square(...arr){
+	// 	let sqr=0;
+	//  for(let i=0;i<arr.length;i++){
+	//  sqr+=Math.pow(arr[i],2)
+	//  }
+	//  console.log(sqr)
+	// }
+	// square(1,2,5,6,12,34)
+
+	// arrayde verilen ededin kesr ve ya tam tipli oldugunu bilmek ucun javascriptde funkisya ve tam edeleri toplasin
+
+	// function float(...arr){
+	// 	let count=0;
+	// 	for(let i=0;i<arr.length;i++){
+	// 		if(typeof arr[i]==='number' && Math.round(arr[i])==arr[i]){
+	// 			count+=arr[i]
+	// 		}
+	// 	}
+	// 	console.log(count)
+	// }
+	// float(1,2.11,3,4.2,5)
+
+	// n-inci funkisya
+	// function show(...arrshow){
+	// 	for (let  i= 0;  i<arrshow.length; i++) {
+	// 		console.log(`Elemet ${i} = ${arrshow[i]}`)
+	// 	}
+	// 	arrshow.shift(1)
+	// }
+
+	// show(1,2,56,78,90)
+
+	// arrayde tekrar olunan elementleri silir
+
+	// function show(...arrshow){
+	// 	let sort=arrshow.sort()
+		
+	// 	for (let i = 0; i < arrshow.length; i++) {
+	// 		if(arrshow[i]==arrshow[i+1]){
+	// 			sort.splice(i,1)
+	// 		}
+	// 	}
+	// 	console.log(sort)	
+	// }
+	// show(1,2,56,78,90,1,3,56)
+	//join
+	// let color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+	// let o = ["th","st","nd","rd"]
+	
+	// for(let i=0;i<color.length;i++){
+		
+	// }
+
+	//
+	// Write a JavaScript program to find the most frequent item of an array.
+	// Sample array: let arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+	// Sample Output: a ( 5 times )
+
+	//alternative
+	// function choose(...Arr){
+
+	// 	let count=0;
+	// 	for (let  i= 0;  i< Arr.length; i++) {
+	// 	if( typeof Arr[i] == "string"){
+	// 		count++
+	// 	}	
+	// 	}
+	// 	console.log(` ${count}`)	
+	//  }
+	//  choose(3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3)
+		let arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+		let mf = 1;
+		let m = 0;
+		let item;
+		for (let i=0; i<arr1.length; i++)
+		{
+			for (let j=i; j<arr1.length; j++)
+			{
+					if (arr1[i] == arr1[j])
+					m++;
+					if (mf<m)
+					{
+					mf=m; 
+					item = arr1[i];
+					}
+			}
+			m=0;
+		}
+		console.log(item+" ( " +mf +" times ) ") ;
+
