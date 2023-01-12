5d1814099611f5605af81d28149f1f028e5440ff
Shahmirze <sahmirze.memmedyarov@gmail.com>
2023-01-13 00:25:00 +0400

    OOP 1

---
+++ jsTasks/OOP1/app.js
@@ -0,0 +1,69 @@
+let arr = [1, 3, 5, 6, 8, 4, 1, 3, 5]
+
+Array.prototype.removeDuplicates = function() {
+    return this.filter((elem, index) => {
+        if (this.indexOf(elem) === index) {
+            return elem
+        }
+    })
+}
+let mix = [null, 12, undefined, NaN, false, 1214, 435, 655, "", 0]
+
+Array.prototype.removeFalsies = function() {
+    return this.filter((elem, index) => {
+        if (!(elem == null || elem == undefined || elem == NaN || elem == 0 || elem == "" || elem == 0)) {
+            return elem
+        }
+    })
+}
+
+Array.prototype.clear = function() {
+    return this.length = 0
+}
+
+
+
+let human = {
+    name: "Sako",
+    age: 19,
+    job: "dev",
+    willing: 'american life'
+}
+Object.prototype.objLength = function() {
+    return Object.keys(this).length
+}
+
+
+Object.prototype.firstProperty = function() {
+    return Object.values(this)[0]
+}
+
+
+Object.prototype.lastProperty = function() {
+    return Object.values(this)[this.objLength() - 1]
+}
+let num = 127
+Number.prototype.isPrime = function() {
+    return this % 2 == 0
+}
+let str = "a12li37"
+String.prototype.toCapitalize = function() {
+    return this[0].toUpperCase() + this.slice(1, this.length)
+}
+
+String.prototype.removeWhiteSpaces = function() {
+    return this.trim()
+}
+
+String.prototype.filterDigits = function() {
+    let arr = []
+    for (let i = 0; i < this.length; i++) {
+        //   if (this[i] * 2 == NaN) {
+
+        if (!this[i] * 2 == NaN) {
+            console.log(this[i])
+        }
+        //    }
+    }
+}
+console.log(str.filterDigits())
